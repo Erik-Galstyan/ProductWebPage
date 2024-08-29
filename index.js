3c985d7fd76d0fd19b57d10abb565d8feed3ea55
Erik-Galstyan <erik.galstyan26@gmail.com>
2024-08-29 14:29:15 +0400

    creating simple product web site

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const {MongoClient} = require("mongodb");
+const bodyParser = require("body-parser");
+const path = require('path');
+const url = "mongodb://127.0.0.1:27017";
+const app = express();
+const client = new MongoClient(url);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+client.connect();
+let db = client.db("Market");
+let items = db.collection("Products");
+app.get("/test", async (req,res) => {
+  const products = await items.find().toArray();
+  res.send(JSON.stringify(products));
+});
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+app.post('/test', async (req, res) => {
+  const { name, price, image } = req.body;
+  const message = await items.insertOne({ name, price, image });
+  res.send("Recived and created");
+});
+app.listen(3010);
